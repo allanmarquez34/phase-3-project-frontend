7179611fc4941c27c0f5729710f85e35e6238d51
Allan Marquez <allan.marquez.2003@icloud.com>
2023-01-03 10:09:59 -0700

    components

---
+++ phase-3-project-frontend/src/components/ActivityPage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ActivityCard from "./ActivityCard"
+
+function ActivityPage(){
+
+    return(
+        <ActivityCard/>
+    )
+    
+}
+
+export default ActivityPage
---
+++ phase-3-project-frontend/src/components/MapPage.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+function MapPage(){
+    
+
+}
+
+export default MapPage
