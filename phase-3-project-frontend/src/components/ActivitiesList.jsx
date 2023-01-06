9b5a94aac254418cd653270d0c40090f96849c82
Allan Marquez <allan.marquez.2003@icloud.com>
2023-01-05 20:40:08 -0700

    new stuff

--- phase-3-project-frontend/src/components/ActivitiesList.jsx
+++ phase-3-project-frontend/src/components/ActivitiesList.jsx
@@ -1,24 +1,16 @@
 import React from "react"
 import ActivityCard from "./ActivityCard"
 
-function ActivitiesList({activities, onActivityDelete, setSearchQuery}){
+function ActivitiesList({activities, onActivityDelete}){
 
     const mappedActivities = activities.map(oneActivity => {
         return <ActivityCard key = {oneActivity.id} activities={oneActivity} onActivityDelete={onActivityDelete}/>
     })
 
-    function handleChange(event){
-        console.log(event.target.value)
-        setSearchQuery(event.target.value)
-    }
-
 
 
     return(
         <div>
-            <h1>Activities</h1>
-            <h3>seach by activity</h3>
-            <input  className="search-bar" type="text" placeholder="Search..." onChange={handleChange}/>
             {mappedActivities}
         </div>
     )
